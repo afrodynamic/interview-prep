1b8b6a4703f80ede92fb2e719946c2db8977902a
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-06-29 12:24:35 -0400

    feat: complete bestTimeToBuyAndSellStock.ts

---
+++ leetcode/top-interview-150/121-best-time-to-buy-and-sell-stock/bestTimeToBuyAndSellStock.test.ts
@@ -0,0 +1,11 @@
+const { maxProfit } = require('./bestTimeToBuyAndSellStock');
+
+describe('maxProfit', () => {
+  test('maxProfit([7,1,5,3,6,4]) should be 5', () => {
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5);
+  });
+
+  test('maxProfit([7,6,4,3,1]) should be 0', () => {
+    expect(maxProfit([7, 6, 4, 3, 1])).toBe(0);
+  });
+});
---
+++ leetcode/top-interview-150/121-best-time-to-buy-and-sell-stock/bestTimeToBuyAndSellStock.ts
@@ -0,0 +1,14 @@
+export function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+  let minPrice = Number.MAX_SAFE_INTEGER;
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i];
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice;
+    }
+  }
+
+  return maxProfit;
+};
