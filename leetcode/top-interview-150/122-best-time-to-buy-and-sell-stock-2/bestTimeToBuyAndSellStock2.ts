79820f71fbd99361d4227f929d5dcca3f936e7d1
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-06-29 12:49:20 -0400

    feat: complete bestTimeToBuyAndSellStock2.ts

---
+++ leetcode/top-interview-150/122-best-time-to-buy-and-sell-stock-2/bestTimeToBuyAndSellStock2.ts
@@ -0,0 +1,13 @@
+export function maxProfit(prices: number[]): number {
+  let maxProfit = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    const profit = prices[i] - prices[i - 1];
+
+    if (profit > 0) {
+      maxProfit += profit;
+    }
+  }
+
+  return maxProfit;
+}
