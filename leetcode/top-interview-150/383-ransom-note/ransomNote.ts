47da79e86a5b634a301845ac911b6bd0adb09e61
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-06-30 11:11:27 -0400

    feat: complete ransomNote.ts

---
+++ leetcode/top-interview-150/383-ransom-note/ransomNote.ts
@@ -0,0 +1,17 @@
+export function canConstruct(ransomNote: string, magazine: string): boolean {
+  const charMap = new Map();
+
+  for (const char of magazine) {
+    charMap.set(char, (charMap.get(char) || 0) + 1);
+  }
+
+  for (const char of ransomNote) {
+    if (!charMap.has(char) || charMap.get(char) === 0) {
+      return false;
+    }
+
+    charMap.set(char, charMap.get(char) - 1);
+  }
+
+  return true;
+};
