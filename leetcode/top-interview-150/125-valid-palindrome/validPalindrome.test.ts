c23a44d39b1687d2648dd8c9c219c8ad6f9acbb7
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-06-30 10:00:56 -0400

    feat: complete validPalindrome.ts

---
+++ leetcode/top-interview-150/125-valid-palindrome/validPalindrome.test.ts
@@ -0,0 +1,15 @@
+const { isPalindrome } = require("./validPalindrome");
+
+describe("isPalindrome", () => {
+  test('"A man, a plan, a canal: Panama" should be true', () => {
+    expect(isPalindrome("A man, a plan, a canal: Panama")).toBe(true);
+  });
+
+  test('"race a car" should be false', () => {
+    expect(isPalindrome("race a car")).toBe(false);
+  });
+
+  test("isPalindrome('') should be true", () => {
+    expect(isPalindrome("")).toBe(true);
+  });
+});
---
+++ leetcode/top-interview-150/125-valid-palindrome/validPalindrome.ts
@@ -0,0 +1,21 @@
+export function isPalindrome(s: string): boolean {
+  let phrase = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  if (phrase.length === 0) {
+    return true;
+  }
+
+  let left = 0;
+  let right = phrase.length - 1;
+
+  while (left < right) {
+    if (phrase[left] !== phrase[right]) {
+      return false;
+    }
+
+    left++;
+    right--;
+  }
+
+  return true;
+};
