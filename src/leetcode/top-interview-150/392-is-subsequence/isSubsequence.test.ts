001566735020f18de8cf22244499141cedc0fa3b
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-10-24 12:58:03 -0400

    feat: add isSubsequence two pointers solution

--- src/leetcode/top-interview-150/392-is-subsequence/isSubsequence.test.ts
+++ src/leetcode/top-interview-150/392-is-subsequence/isSubsequence.test.ts
@@ -1,27 +1,29 @@
-import { isSubsequence } from './isSubsequence';
+import { isSubsequence, isSubsequenceIndexOf } from './isSubsequence';
 
-describe('isSubsequence', () => {
+const functionsToTest = [{ fn: isSubsequence }, { fn: isSubsequenceIndexOf }];
+
+describe.each(functionsToTest)('%s', ({ fn }) => {
   test('returns true if s is a subsequence of t is true', () => {
-    expect(isSubsequence('abc', 'ahbdgdc')).toBe(true);
+    expect(fn('abc', 'ahbdgdc')).toBe(true);
   });
 
   test('returns false if the first letter of s is missing in t', () => {
-    expect(isSubsequence('xahbdgdc', 'ahbdgdc')).toBe(false);
+    expect(fn('xahbdgdc', 'ahbdgdc')).toBe(false);
   });
 
   test('returns false if s and t are single letters where s !== t', () => {
-    expect(isSubsequence('a', 'b')).toBe(false);
+    expect(fn('a', 'b')).toBe(false);
   });
 
   test('returns false if an intermediate letter of s is missing in t', () => {
-    expect(isSubsequence('axc', 'ahbgdc')).toBe(false);
+    expect(fn('axc', 'ahbgdc')).toBe(false);
   });
 
   test('returns true for empty s string', () => {
-    expect(isSubsequence('', 'ahbdgdc')).toBe(true);
+    expect(fn('', 'ahbdgdc')).toBe(true);
   });
 
   test('returns true for empty t string', () => {
-    expect(isSubsequence('ahbdgdc', '')).toBe(false);
+    expect(fn('ahbdgdc', '')).toBe(false);
   });
 });
--- src/leetcode/top-interview-150/392-is-subsequence/isSubsequence.ts
+++ src/leetcode/top-interview-150/392-is-subsequence/isSubsequence.ts
@@ -1,4 +1,18 @@
 export const isSubsequence = (s: string, t: string): boolean => {
+  let sPointer = 0;
+  let tPointer = 0;
+
+  while (sPointer < s.length && tPointer < t.length) {
+    if (s[sPointer] === t[tPointer]) {
+      sPointer++;
+    }
+    tPointer++;
+  }
+
+  return sPointer === s.length;
+};
+
+export const isSubsequenceIndexOf = (s: string, t: string): boolean => {
   let index = -1;
 
   for (let i = 0; i < s.length; i++) {
